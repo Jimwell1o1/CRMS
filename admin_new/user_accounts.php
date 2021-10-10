8b5d5ccd1671d6ca2b6f4d22e31d568e671022b9
Jimwell1o1 <82427374+Jimwell1o1@users.noreply.github.com>
2021-10-11 01:54:11 +0800

    Update user_accounts.php

--- admin_new/user_accounts.php
+++ admin_new/user_accounts.php
@@ -22,12 +22,18 @@
       href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css"
       rel="stylesheet"
     />
-    
+
     <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" rel="stylesheet" />
     <script
       src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"
       crossorigin="anonymous"
     ></script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 
       <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -44,180 +50,8 @@
 <link href="../assets/css/style.css" rel="stylesheet">
   </head>
   <body class="sb-nav-fixed">
-    <!--========= NAV BAR =========-->
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-      <!-- Navbar Brand-->
-      <a class="navbar-brand ps-3" href="../index.php">MCY Dental Clinic</a>
-      <!-- Sidebar Toggle-->
-      <button
-        class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
-        id="sidebarToggle"
-        href="#!"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
-      <!-- Navbar Search-->
-      <form
-        class="
-          d-none d-md-inline-block
-          form-inline
-          ms-auto
-          me-0 me-md-3
-          my-2 my-md-0
-        "
-      >
-        <div class="input-group">
-          <input
-            class="form-control"
-            type="text"
-            placeholder="Search for..."
-            aria-label="Search for..."
-            aria-describedby="btnNavbarSearch"
-            id="btnNavbarSearch"
-            onchange="openPage()"
-          />
-          <button class="btn btn-primary" id="btnNavbarSearch" type="button">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </form>
 
-      <!-- Script for Searching Page -->
-     <script>
-       function openPage(){
-         var x =document.getElementById("btnNavbarSearch").value;
-
-         if(x === "pending"){
-           window.open("pending_tables.html");
-         }
-         if(x == "declined" || "DECLINED"){
-          window.open("declined_tables.html");
-         }
-         else if(x == "accepted" || "ACCEPTED"){
-          window.open("accepted_tables.html");
-         }
-         else if(x == "history" || "HISTORY"){
-          window.open("customer_history.html");
-         }
-         else if(x == "patient" || "PATIENT"){
-          window.open("add_patient.html");
-         }
-       }
-     </script>
-
-      <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            id="navbarDropdown"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            ><i class="fas fa-user fa-fw"></i
-          ></a>
-          <ul
-            class="dropdown-menu dropdown-menu-end"
-            aria-labelledby="navbarDropdown"
-          >
-            <li><a class="dropdown-item" href="#!">Settings</a></li>
-            <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#!">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-    <div id="layoutSidenav">
-      <div id="layoutSidenav_nav">
-        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-          <div class="sb-sidenav-menu">
-            <div class="nav">
-              <div class="sb-sidenav-menu-heading">General</div>
-              <a class="nav-link" href="index.php">
-                <div class="sb-nav-link-icon">
-
-                
-                <i class="fas fa-home"></i>
-                </div>
-                Home
-              </a>
-
-              <a class="nav-link" href="index.php">
-                <div class="sb-nav-link-icon">
-
-                
-                  <i class="fas fa-tachometer-alt"></i>
-                </div>
-                Dashboard
-              </a>
-              <div class="sb-sidenav-menu-heading">Function</div>
-
-              <a
-                class="nav-link collapsed"
-                href="#"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseSched"
-                aria-expanded="false"
-                aria-controls="collapseSched"
-              >
-                <div class="sb-nav-link-icon">
-                  <i class="fas fa-columns"></i>
-                </div>
-                Appointments
-                <div class="sb-sidenav-collapse-arrow">
-                  <i class="fas fa-angle-down"></i>
-                </div>
-              </a>
-              <div
-                class="collapse"
-                id="collapseSched"
-                aria-labelledby="headingOne"
-                data-bs-parent="#sidenavAccordion"
-              >
-                <nav class="sb-sidenav-menu-nested nav">
-                  <a class="nav-link" href="pending_tables.php"
-                    >Pending Schedules</a
-                  >
-                  <a class="nav-link" href="accepted_tables.php"
-                    >Accepted Schedules</a
-                  >
-                  <a class="nav-link" href="declined_tables.php"
-                    >Decline Schedules</a
-                  >
-                  <a class="nav-link" href="declined_tables.php"
-                    >Missed Schedules</a
-                  >
-                  <a class="nav-link" href="customer_history.php"
-                    >Customer History</a
-                  >
-                </nav>
-              </div>
-              <div class="sb-sidenav-menu-heading">Users</div>
-              <a class="nav-link" href="user_accounts.php">
-                <div class="sb-nav-link-icon">
-                <i class="fas fa-archive"></i>
-                </div>
-                Accounts
-              </a>
-
-              <a class="nav-link" href="add_patient.php">
-                <div class="sb-nav-link-icon">
-                  <i class="fas fa-plus-circle"></i>
-                </div>
-                Add Patient
-              </a>
-
-              
-          <div class="sb-sidenav-footer">
-            <div class="small">Logged in as:</div>
-            Administrator
-          </div>
-        </nav>
-      </div>
-
-
-      <!--========= END OF NAV BAR =========-->
+  <?php include "includes/navbar.php"; ?> <!--==== NAV BAR ====-->
 
       <div id="layoutSidenav_content">
         <main>
@@ -236,10 +70,10 @@
                 Accounts
               </div>
               <div class="card-body">
-                <table id="datatablesSimple">
+                <table id="example" class="table table-stripped table-bordered">
 
                 <?php
-                            $sql = "SELECT * FROM booking;";
+                            $sql = "SELECT * FROM users;";
                             $result = mysqli_query($conn, $sql);
                             $resultChecked = mysqli_num_rows($result);
 ?>  
@@ -249,22 +83,21 @@
         <th scope="col">Name</th>
         <th scope="col">Username</th>
         <th scope="col">Email</th>
-        <th scope="col">Status</th>
         <th scope="col">Action</th>
         </tr>
         </thead>
         <?php
                             if($resultChecked > 0){  
                                 while($row = mysqli_fetch_assoc($result)){
-                                    if("Accepted" === $row['bookingStatus']){ ?>
+                                     ?>
                                   <tbody>
                                             <tr>
-                                            <td> <?php echo '1' ?> </td>
-                                            <th scope="row"> <?php echo $row['bookingName'] ?> </th>
                                             
-                                                <td> <?php echo $row['bookingGender'] ?> </td>
-                                                <td> <?php echo 'rjosdjnsa@gmail.com' ?> </td>
-                                                <td> <?php echo $row['bookingDate'] ?> </td>
+                                            <th scope="row"> <?php echo $row['usersId'] ?> </th>
+                                            
+                                                <td> <?php echo $row['usersName'] ?> </td>
+                                                <td> <?php echo $row['usersEmail'] ?> </td>
+                                                <td> <?php echo $row['usersUid'] ?> </td>
 
                                             
                                                 
@@ -272,10 +105,10 @@
                                                     <form action="../includes/updatePendingData.php?bookingId=<?php echo htmlspecialchars($row['bookingId'])?>" method="POST">
                                                         <!-- <button class="btn btn-outline-primary"id="accept-button" name="submit"> Update </button> -->
                                                         <button type="button" class="btn btn-primary" id="accept-button" name="submit">
-                                                          <i class="fas fa-edit"></i> Edit
+                                                          <i class="fas fa-edit"></i> 
                                                         </button> 
                                                         <button type="button" class="btn btn-danger" id="accept-button" name="submit">
-                                                          <i class="fas fa-trash"></i> Delete
+                                                          <i class="fas fa-trash"></i>
                                                         </button> 
                                                   
                                                     </form>
@@ -283,7 +116,7 @@
                                                 </tbody>
                                        
 
-                            <?php } } } ?>
+                            <?php } }  ?>
                 
                  
                   </tbody>
@@ -310,6 +143,20 @@
         </footer>
       </div>
     </div>
+    <script>
+      $(document).ready(function() {
+    $('#example').DataTable();
+    
+} );
+
+
+
+
+
+
+
+
+</script>
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
       crossorigin="anonymous"
