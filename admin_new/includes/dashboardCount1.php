d016e73f91efb5b3cf3c82d2621b67827a55882c
Jimwell1o1 <rjohnjimwell@gmail.com>
2021-10-15 01:19:16 +0800

    dashboard count

---
+++ admin_new/includes/dashboardCount1.php
@@ -0,0 +1,37 @@
+<?php
+$conn = mysqli_connect("localhost","root","","phpproject01");
+$sessionbranch = $_SESSION['admin_branchName'];
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Pending' AND bookingBranch = '".$sessionbranch."'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $pending = $row['bookingCount'];
+}
+
+
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Accepted' AND bookingBranch = '".$sessionbranch."'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $accepted = $row['bookingCount'];
+}
+
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Declined' AND bookingBranch = '".$sessionbranch."'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $decline = $row['bookingCount'];
+}
+
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Done' AND bookingBranch = '".$sessionbranch."'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $done = $row['bookingCount'];
+}
+
+?>
---
+++ admin_new/includes/dashboardCount2.php
@@ -0,0 +1,37 @@
+<?php
+$conn = mysqli_connect("localhost","root","","phpproject01");
+
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Pending'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $pending = $row['bookingCount'];
+}
+
+
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Accepted'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $accepted = $row['bookingCount'];
+}
+
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Declined'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $decline = $row['bookingCount'];
+}
+
+$query = "SELECT COUNT(*) as bookingCount FROM booking WHERE bookingStatus = 'Done'";
+
+$query_result = mysqli_query($conn,$query);
+while($row = mysqli_fetch_assoc($query_result)){
+
+  $done = $row['bookingCount'];
+}
+
+?>
