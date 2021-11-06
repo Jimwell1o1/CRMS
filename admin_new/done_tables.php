753acb01118d48113ec21ed76a4dd38406171b43
John Carlo Paclibar <78149513+jcpac0250@users.noreply.github.com>
2021-11-05 22:50:48 -0800

    Adding missed sched and doned sched

--- admin_new/done_tables.php
+++ admin_new/done_tables.php
@@ -11,6 +11,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta
@@ -19,34 +20,37 @@
     />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Tables - SB Admin</title>
+    <title>Accepted - Admin</title>
     <?php
         include 'includes/style-links.php';
     ?>
+
   </head>
   <body class="sb-nav-fixed">
-    
-  <?php include "includes/navbar.php"; ?> <!--==== NAV BAR ====-->
 
-    
+    <?php include "includes/navbar.php"; ?> <!--==== NAV BAR ====-->
+
+
       <div id="layoutSidenav_content">
         <main>
           <div class="container-fluid px-4">
-            <h1 class="mt-4">Missed Schedule</h1>
+            <h1 class="mt-4">Done Schedule</h1>
             <ol class="breadcrumb mb-4">
-              <li class="breadcrumb-item">&nbsp;&nbsp;
+              <li class="breadcrumb-item">
                 <a href="index.php">Dashboard</a>
               </li>
-              <li class="breadcrumb-item active">Missed Schedules</li>
+              <li class="breadcrumb-item active">Done Schedule</li>
             </ol>
 
             <div class="card mb-4">
               <div class="card-header">
                 <i class="fas fa-table me-1"></i>
-                Missed
+                Accepted
               </div>
               <div class="card-body">
-                <table id="datatablesSimple">
+               
+
+              <table id="datatablesSimple">
                   <thead>
                     <tr>
                       <th>Name</th>
@@ -79,55 +83,34 @@
 
                             if($resultChecked > 0){  
                                 while($row = mysqli_fetch_assoc($result)){
-                                    if("Missed" === $row['bookingStatus']){ 
+                                    if("Done" === $row['bookingStatus']){ 
                                       if($_SESSION['admin_branchName'] === $row['bookingBranch']){ 
-                                        include 'includes/tables/missed_tables.inc.php';
-
-                                       } 
+                                             include 'includes/tables/doned_tables.inc.php';
+                                } 
                                 if($_SESSION['admin_branchName'] === "mainAdmin"){
-                                        include 'includes/tables/missed_tables.inc.php';
-                            }
-                          } 
-                        } 
-                      } 
-                      
-                  ?>
+                                 
+                                  include 'includes/tables/doned_tables.inc.php';
+                         
+                          } } } } ?>
+
                   </tbody>
                 </table>
               </div>
             </div>
           </div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-        </main>
+                        </main>
           <?php 
           include 'modalAddpatient.php';
 
           ?>
+
+
         <footer class="py-4 bg-light mt-auto">
           <div class="container-fluid px-4">
             <div
               class="d-flex align-items-center justify-content-between small"
             >
-              <div class="text-muted">Copyright &copy; Your Website 2021</div>
+              <div class="text-muted">Copyright &copy; MCY Dental Clinic 2021</div>
               <div>
                 <a href="#">Privacy Policy</a>
                 &middot;
@@ -135,11 +118,10 @@
               </div>
             </div>
           </div>
-      
-
         </footer>
       </div>
     </div>
+    
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
       crossorigin="anonymous"
@@ -151,5 +133,16 @@
     ></script>
     <script src="js/datatables-simple-demo.js"></script>
 
+    <script>
+      function ConfirmDelete(){
+
+  var x = confirm("Are you sure you want to delete?");
+  if (x)
+      return true;
+  else
+    return false;
+    }
+    </script>
+    
   </body>
 </html>
