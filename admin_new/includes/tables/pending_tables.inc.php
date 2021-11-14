a6fc91d12cf32683eb40f83e77f040510a843459
Jimwell1o1 <rjohnjimwell@gmail.com>
2021-11-15 00:45:18 +0800

    Update pending_tables.inc.php

--- admin_new/includes/tables/pending_tables.inc.php
+++ admin_new/includes/tables/pending_tables.inc.php
@@ -47,27 +47,159 @@
         </div>
         <div>
           <label class="form-label">Gender:</label>
-          <input name="gender" type="text" class="form-control" value="<?php echo $row['bookingGender']; ?>"  required>
+          <select name="gender" class="form-control" class="input-field box">
+              <option value="" disabled>Select Gender:</option>
+
+              <!-- TO KEEP THE DETAILS TYPED Malinao-------->        
+              <option value = "Male" <?php
+                if($row['bookingGender'] == 'Male'){
+                    echo ' selected="selected"';
+                    }?>>Male</option>
+
+              <option value = "Female" <?php
+                if($row['bookingGender'] == 'Female'){
+                    echo ' selected="selected"';
+                    }?>>Female</option>
+        </select>
         </div>
         <div>
           <label class="form-label">Appointment Date:</label>
-          <input name="date" type="text" class="form-control" value="<?php echo $row['bookingDate']; ?>"  required>
+          <input name="date" type="date" class="form-control" value="<?php echo $row['bookingDate']; ?>"  required>
         </div>
         <div>
           <label class="form-label">Time:</label>
-          <input name="time" type="text" class="form-control" value="<?php echo $row['bookingTime']; ?>" required>
+          <select name="time" class="form-control" class="input-field box">
+
+              <option value="" disabled>Select Time:</option>
+
+              <!-- TO KEEP THE DETAILS TYPED Malinao-------->        
+              <option value = "8:00 AM" <?php
+                if($row['bookingTime'] == '8:00 AM'){
+                    echo ' selected="selected"';
+                    }?>>8:00 AM</option>
+              <option value = "8:00 AM" <?php
+                if($row['bookingTime'] == '8:00 AM'){
+                    echo ' selected="selected"';
+                    }?>>9:00 AM</option>
+              <option value = "8:00 AM" <?php
+                if($row['bookingTime'] == '9:00 AM'){
+                    echo ' selected="selected"';
+                    }?>>9:00 AM</option>
+              <option value = "10:00 AM" <?php
+                if($row['bookingTime'] == '10:00 AM'){
+                    echo ' selected="selected"';
+                    }?>>10:00 AM</option>
+              <option value = "1:00 PM" <?php
+                if($row['bookingTime'] == '1:00 PM'){
+                    echo ' selected="selected"';
+                    }?>>1:00 PM</option>
+              <option value = "2:00 PM" <?php
+                if($row['bookingTime'] == '2:00 PM'){
+                    echo ' selected="selected"';
+                    }?>>2:00 PM</option>
+              <option value = "3:00 PM" <?php
+                if($row['bookingTime'] == '3:00 PM'){
+                    echo ' selected="selected"';
+                    }?>>3:00 PM</option>
+              <option value = "4:00 PM" <?php
+                if($row['bookingTime'] == '4:00 PM'){
+                    echo ' selected="selected"';
+                    }?>>4:00 PM</option>
+
+
+              </select>
         </div>
         <div>
           <label class="form-label">Dental Clinic:</label>
-          <input name="branch" type="text" class="form-control" value="<?php echo $row['bookingBranch']; ?>"  required>
+          <select name="branch" class="form-control" class="input-field box">
+
+                            <option value="" disabled>Select Branch:</option>
+
+                             <!-- TO KEEP THE DETAILS TYPED Malinao-------->        
+                             <option value = "Malinao" <?php
+                               if($row['bookingBranch'] == 'Malinao'){
+                                  echo ' selected="selected"';
+                                  }?>>Malinao</option>
+
+                              <option value = "Pinagbuhatan" <?php
+                               if($row['bookingBranch'] == 'Pinagbuhatan'){
+                                  echo ' selected="selected"';
+                                  }?>>Pinagbuhatan</option>
+
+                              <option value = "San Joaquin" <?php
+                               if($row['bookingBranch'] == 'San Joaquin'){
+                                  echo ' selected="selected"';
+                                  }?>>San Joaquin</option>
+
+                        </select>
         </div>
         <div>
           <label class="form-label">Procedure/Dental Service:</label>
-          <input name="procedure" type="text" class="form-control" value="<?php echo $row['bookingConsultation']; ?>"  required>
+          <select name="procedure" class="form-control" class="input-field box">
+
+                            <option value="" disabled>Select Procedure:</option>
+
+                             <!-- TO KEEP THE DETAILS TYPED Malinao-------->        
+                             <option value = "Consultation" <?php
+                               if($row['bookingConsultation'] == 'Consultation'){
+                                  echo ' selected="selected"';
+                                  }?>>Consultation</option>
+                              <option value = "Oral Prophylaxis" <?php
+                               if($row['bookingConsultation'] == 'Oral Prophylaxis'){
+                                  echo ' selected="selected"';
+                                  }?>>Oral Prophylaxis</option>
+                              <option value = "Tooth Restoration" <?php
+                               if($row['bookingConsultation'] == 'Tooth Restoration'){
+                                  echo ' selected="selected"';
+                                  }?>>Tooth Restoration</option>
+                              <option value = "Tooth Extraction" <?php
+                               if($row['bookingConsultation'] == 'Tooth Extraction'){
+                                  echo ' selected="selected"';
+                                  }?>>Tooth Extraction</option>
+                              <option value = "Fluoride Application" <?php
+                               if($row['bookingConsultation'] == 'Fluoride Application'){
+                                  echo ' selected="selected"';
+                                  }?>>Fluoride Application</option>
+                              <option value = "Prosthodontic Treatment" <?php
+                               if($row['bookingConsultation'] == 'Prosthodontic Treatment'){
+                                  echo ' selected="selected"';
+                                  }?>>Prosthodontic Treatment</option>
+                               <option value = "Orthodontic Treatment" <?php
+                               if($row['bookingConsultation'] == 'Orthodontic Treatment'){
+                                  echo ' selected="selected"';
+                                  }?>>Orthodontic Treatment</option>
+                               <option value = "Periodontic Rehab" <?php
+                               if($row['bookingConsultation'] == 'Periodontic Rehab'){
+                                  echo ' selected="selected"';
+                                  }?>>Periodontic Rehab</option>
+
+                        </select>
         </div>
         <div>
           <label class="form-label">Status:</label>
-          <input name="status" type="text" class="form-control" value="<?php echo $row['bookingStatus']; ?>"  required>
+          <select name="status" class="form-control" class="input-field box">
+
+                            <option value="" disabled>Select Status:</option>
+
+                             <!-- TO KEEP THE DETAILS TYPED Malinao-------->        
+                             <option value = "Pending" <?php
+                               if($row['bookingStatus'] == 'Pending'){
+                                  echo ' selected="selected"';
+                                  }?>>Pending</option>
+                             <option value = "Accepted" <?php
+                               if($row['bookingStatus'] == 'Accepted'){
+                                  echo ' selected="selected"';
+                                  }?>>Accepted</option>
+                             <option value = "Declined" <?php
+                               if($row['bookingStatus'] == 'Declined'){
+                                  echo ' selected="selected"';
+                                  }?>>Declined</option>  
+                             <option value = "Done" <?php
+                               if($row['bookingStatus'] == 'Done'){
+                                  echo ' selected="selected"';
+                                  }?>>Done</option>  
+
+                        </select>
         </div>
    
       </div>
