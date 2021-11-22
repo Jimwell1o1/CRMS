4de63c994ad2cc941579b6084d5044ac83a1df54
Jimwell1o1 <82427374+Jimwell1o1@users.noreply.github.com>
2021-11-22 19:37:54 +0800

    fonts

--- policy/privacy.php
+++ policy/privacy.php
@@ -20,7 +20,7 @@
   <link href="../assets/appointmentAssets/img/apple-touch-icon.PNG" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="../assets/appointmentAssets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
