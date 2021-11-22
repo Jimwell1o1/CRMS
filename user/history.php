81844dcfe7aa9070f22a5b2cd0078adfed5a3f9b
John Carlo Paclibar <78149513+jcpac0250@users.noreply.github.com>
2021-11-22 02:01:56 -0900

    Terms new added

--- user/history.php
+++ user/history.php
@@ -23,6 +23,29 @@
   <meta content="" name="descriptison">
   <meta content="" name="keywords">
 
+  <style>
+    .dtHorizontalVerticalExampleWrapper {
+      max-width: 600px;
+      margin: 0 auto;
+    }
+    
+    #dtHorizontalVerticalExample th,
+    td {
+      white-space: nowrap;
+    }
+    table.dataTable thead .sorting:after,
+    table.dataTable thead .sorting:before,
+    table.dataTable thead .sorting_asc:after,
+    table.dataTable thead .sorting_asc:before,
+    table.dataTable thead .sorting_asc_disabled:after,
+    table.dataTable thead .sorting_asc_disabled:before,
+    table.dataTable thead .sorting_desc:after,
+    table.dataTable thead .sorting_desc:before,
+    table.dataTable thead .sorting_desc_disabled:after,
+    table.dataTable thead .sorting_desc_disabled:before {
+    bottom: 0.5em;
+    }
+  </style>
   <!-- Favicons -->
   <link href="../assets/img/favicon.png" rel="icon">
   <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
@@ -100,7 +123,9 @@
             } 
           }
             ?>
-        <table class="table" data-aos="fade-right">
+            
+        <table id="dtHorizontalVerticalExample" class="table table-striped table-bordered table-sm " cellspacing="0"
+  width="100%" data-aos="fade-right">
                                     <thead class="thead-light">
                                             <tr>
                                             <th scope="col">Patient</th>
@@ -170,7 +195,11 @@
                             </table>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> 
                                                 </div></div></div>
+
+
+  
     <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
   <!-- Vendor JS Files -->
   <script src="../assets/vendor/jquery/jquery.min.js"></script>
   <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -184,6 +213,7 @@
   <!-- Template Main JS File -->
   <script src="../assets/js/main.js"></script>
 
+  
 
   <script>
     function ConfirmDelete(){
